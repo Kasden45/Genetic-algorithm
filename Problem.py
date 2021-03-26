68b6dca183f09544d0d1decb0b9cea9a998838ed
Student246752 <246752@student.pwr.edu.pl>
2021-03-26 17:41:06 +0100

    Add comments

--- Problem.py
+++ Problem.py
@@ -1,4 +1,5 @@
 import copy
+import pickle
 import random
 
 from Individual import Individual
@@ -6,13 +7,13 @@ from PCB_Board import PCB_Board
 from Populations import Population
 from Parameters import *
 collisions_weight = 100
-total_len_weight = 4
-total_segments_weight = 1
+total_len_weight = 15
+total_segments_weight = 5
 out_weight = 200
 len_out_weight = 100
 
 class Problem:
-    def __init__(self, px=0.5, pm=0.5, size=1000, iterations=5, selection="Tournament", board_file="zad1.txt", n=30):
+    def __init__(self, px=0.5, pm=0.8, size=300, iterations=55, selection="Tournament", board_file="zad3.txt", n=0.1):
         self.px = px
         self.pm = pm
         self.all_populations = []
@@ -24,16 +25,30 @@ class Problem:
         self.best_solution = None
         self.tournament_n = n
 
+
     def crossover_operator(self, p1: Individual, p2: Individual, change=1):
+        """
+
+        :param p1: parent1
+        :param p2: parent2
+        :param change:
+        :return: children generated from parents genes
+        """
         new_traces_1 = []
         new_traces_2 = []
         for i in range(len(p1.traces)):
-            if i == change: # random.random() < self.px
-                new_traces_1.append(copy.deepcopy(p2.traces[i]))
-                new_traces_2.append(copy.deepcopy(p1.traces[i]))
+            if random.random() < self.px:
+                # pickle not recommended, but faster
+                new_traces_1.append(pickle.loads(pickle.dumps(p2.traces[i])))
+                new_traces_2.append(pickle.loads(pickle.dumps(p1.traces[i])))
+                # new_traces_1.append(copy.deepcopy(p2.traces[i]))
+                # new_traces_2.append(copy.deepcopy(p1.traces[i]))
             else:
-                new_traces_1.append(copy.deepcopy(p1.traces[i]))
-                new_traces_2.append(copy.deepcopy(p2.traces[i]))
+                # pickle not recommended, but faster
+                new_traces_1.append(pickle.loads(pickle.dumps(p1.traces[i])))
+                new_traces_2.append(pickle.loads(pickle.dumps(p2.traces[i])))
+                # new_traces_1.append(copy.deepcopy(p1.traces[i]))
+                # new_traces_2.append(copy.deepcopy(p2.traces[i]))
 
         child1 = Individual(p1.board, traces=new_traces_1)
 
@@ -41,8 +56,16 @@ class Problem:
         return child1, child2
 
     def mutation_operator(self, child, crossed, pm):
+        """
+
+        :param child: individual to be mutated
+        :param crossed: indicates if child was generated from two parents or it's just a parent
+        :param pm: mutation probability
+        :return: mutated individual
+        """
         if not crossed:
-            mutable = copy.deepcopy(child)
+            mutable = pickle.loads(pickle.dumps(child))
+            #mutable = copy.deepcopy(child)
         else:
             mutable = child
         for i in range(len(mutable.traces)):
@@ -52,6 +75,7 @@ class Problem:
                 segment_index = random.randint(0, len(mutable.traces[i].segments[:-1])-1)
                 if random.random() < 0.5:
                     # Extend by 1
+                    pass
                     mutable.traces[i].lengthen_segment(segment_index)
                 else:
                     pass
@@ -60,7 +84,25 @@ class Problem:
 
         return mutable
 
+    def solve_problem_randomly(self):
+        """
+        Function that solves problem with randomly generated solutions
+        :return:
+        """
+        random_population = Population()
+        random_population.set_fitness(collisions_weight, total_len_weight, total_segments_weight, out_weight,
+                                        len_out_weight)
+        random_population.generate_population(self.board, self.population_size)
+        random_population.grade_population()
+        self.best_solution = (
+        copy.deepcopy(random_population.best_individual()), random_population.fitness_ranking[1][1])
+        return random_population.fitness_ranking
+
     def solve_problem(self):
+        """
+        Function that solves problem with genetic algorithm
+        :return: best solution, fitness object
+        """
         previous_population = Population()
         previous_population.set_fitness(collisions_weight, total_len_weight, total_segments_weight, out_weight,
                                         len_out_weight)
@@ -91,7 +133,8 @@ class Problem:
                     child1, child2 = self.crossover_operator(p1, p2)
                     crossed = True
                 else:
-                    child1 = copy.deepcopy(random.choice([p1, p2]))
+                    child1 = pickle.loads(pickle.dumps(random.choice([p1, p2])))
+                    #child1 = copy.deepcopy(random.choice([p1, p2]))
 
                 new_population.add_individual(self.mutation_operator(child1, crossed, self.pm))
                 if child2 is not None and len(new_population.population) < self.population_size:
